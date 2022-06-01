3e15d28ddd064829d41ff5363ce80a2648e2ff68
Mr_Tower <68717122+pedromlabio@users.noreply.github.com>
2022-06-01 08:19:19 -0400

    adicionado novo exercício em Python
    exercício 1184 adicionado em Python

---
+++ python/1184.py
@@ -0,0 +1,36 @@
+def criarMatriz():
+    matriz = []
+
+    for i in range(12):
+        matriz.append([])
+        for j in range(12):
+            valor = float(input())
+            matriz[i].append(valor)
+
+    return matriz
+
+def somar(matriz):
+    soma = 0
+    for i in range(12):
+        for j in range(i + 1, 12):
+            soma+= matriz[j][i]
+
+    return soma
+
+def media(matriz):
+    soma = somar(matriz)
+    return soma/66
+
+
+def main():
+    opcao = input()
+    matriz = criarMatriz()
+
+    if opcao == "S":
+        resultado = somar(matriz)
+    elif opcao == "M":
+        resultado = media(matriz)
+
+    print(f"{resultado:.1f}")
+
+main()
