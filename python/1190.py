40c6ba590b425afd21d86585e0d96357994ccc27
pedromlabio <68717122+pedromlabio@users.noreply.github.com>
2022-06-01 16:35:26 -0400

    ex 1190 adicionado porém não resolvido em python

---
+++ python/1190.py
@@ -0,0 +1,41 @@
+def criarMatriz():
+    matriz = []
+
+    for i in range(12):
+        matriz.append([])
+        for j in range(12):
+            valor = float(input())
+            matriz[i].append(valor)
+
+    return matriz
+
+def somar(matriz):
+    soma = 0
+    for i in range(5):
+        for j in range(11, 12 - i, 1):
+            soma+= matriz[j][i]
+
+    for i in range(6, 12):
+        for j in range(10 - i, -1, -1):
+            soma+= matriz[i][j]
+
+
+    return soma
+
+def media(matriz):
+    soma = somar(matriz)
+    return soma/30
+
+
+def main():
+    opcao = input()
+    matriz = criarMatriz()
+
+    if opcao == "S":
+        resultado = somar(matriz)
+    elif opcao == "M":
+        resultado = media(matriz)
+
+    print(f"{resultado:.1f}")
+
+main()
