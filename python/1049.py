734b619b0b160242be352fc06d9b8af92ffc189b
pedromlabio <68717122+pedromlabio@users.noreply.github.com>
2023-03-26 23:09:54 -0400

    ex 1049 e 1050 em python

---
+++ python/1049.py
@@ -0,0 +1,48 @@
+def main():
+
+
+    w1 = input()
+    w2 = input()
+    w3 = input()
+
+
+
+    if(w1 == "vertebrado"):
+
+        if(w2 == "ave"):
+
+            if(w3 == "carnivoro"):
+                print("aguia")
+            elif(w3 == "onivoro"):
+                print("pomba")
+
+        elif(w2 == "mamifero"):
+
+            if(w3 == "onivoro"):
+                print("homem")
+            elif(w3 == "herbivoro"):
+                print("vaca")
+
+    elif(w1 == "invertebrado"):
+
+        if(w2 == "inseto"):
+
+            if(w3 == "hematofago"):
+                print("pulga")
+            elif(w3 == "herbivoro"):
+                print("lagarta")
+
+        elif(w2 == "anelideo"):
+
+            if(w3 == "hematofago"):
+                print("sanguessuga")
+            elif(w3 == "onivoro"):
+                print("minhoca")
+
+
+
+
+
+
+#==========================
+main()
