f824f198e7556754fd8430b28120389a5221d2b4
pedromlabio <68717122+pedromlabio@users.noreply.github.com>
2023-03-30 22:54:59 -0400

    ex 1116 em python

---
+++ python/1116.py
@@ -0,0 +1,11 @@
+def main():
+    n = int(input())
+
+    for i in range(n):
+        a, b = map(int, input().split())
+        if (b == 0):
+            print("divisao impossivel")
+        else:
+            print(f"{a/b:.1f}")
+
+main()
